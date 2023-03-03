38b986a5e090d2eb12618950b024f0b27fbdea8e
Déborah <melody.juvin@gmail.com>
2023-03-03 21:14:22 +0100

    add class active

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -6,36 +6,54 @@ async function getElementAPI() {
             let imgUrl = element.imageUrl;
             let titleimg = element.title;
             document.getElementById("imgProjet").innerHTML +=
-            `<figure class="projectItem" data-figure=${element.categoryId}>
+                `<figure class="projectItem" data-figure=${element.categoryId}>
                 <img src=${imgUrl} alt=${titleimg}>
                 <figcaption>${titleimg}</figcaption>
-            </figure>` 
-        });           
-            const buttonFilter = document.querySelectorAll('button');
-            const projectItem = document.getElementsByClassName("projectItem");
-         
-            for (let a = 0; a < buttonFilter.length; a++) {
-     
-                buttonFilter[a].addEventListener('click', event => {
-            
-                    const buttonfilterdatasetId = event.target.dataset.id;
-                   
-                    [...projectItem].forEach(item =>{    
+            </figure>`
+        });
+        const buttonFilter = document.querySelectorAll('button');
+        const projectItem = document.getElementsByClassName("projectItem");
+
+
+        for (let a = 0; a < buttonFilter.length; a++) {
+
+            buttonFilter[a].addEventListener('click', event => {
+
+                const buttonfilterdatasetId = event.target.dataset.id;
+
+
+                [...projectItem].forEach(item => {
+
                     const dataCategory = item.dataset.figure;
-                       
-                        if (buttonfilterdatasetId === dataCategory || buttonfilterdatasetId==0) {
-                            item.style.display = "initial";
-                        }
-                        else {
-                            item.style.display = "none";
-                        }
-                    })
-                 
+
+                    if (buttonfilterdatasetId === dataCategory || buttonfilterdatasetId == 0) {
+
+                        item.style.display = "initial";
+                    }
+                    else {
+
+                        item.style.display = "none";
+                    }
                 })
-            }
-    
+
+                for (let b = 0; b < buttonFilter.length; b++) {
+
+                    if (buttonFilter[b].dataset.id === event.target.dataset.id) {
+
+                        buttonFilter[b].classList.add("active");
+                    }
+                    else {
+
+                        buttonFilter[b].classList.remove("active");
+                    }
+
+                }
+
+            })
+        }
+
     } catch (err) {
-        console.error(err);        
+        console.error(err);
     }
 };
 getElementAPI()
