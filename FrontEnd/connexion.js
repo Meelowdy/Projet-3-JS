8629727410ab6e9449e8775c3005da398f44bb72
Déborah <melody.juvin@gmail.com>
2023-03-20 16:36:23 +0100

    update/add CSS and login form

---
+++ FrontEnd/connexion.js
@@ -0,0 +1,6 @@
+const connexionButton = document.getElementsByClassName("buttonConnexion")
+connexionButton.addEventListener("click", (event) => {
+    event.preventDefault();
+let emailForm = document.formLogin.email.value;
+let passwordForm = document.formLogin.password.value;
+}) 
