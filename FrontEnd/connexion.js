9692f4a573b72982d17c0b42891049cac556f145
Déborah <melody.juvin@gmail.com>
2023-03-20 19:40:33 +0100

    recovery token with form

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,6 +1,23 @@
-const connexionButton = document.getElementsByClassName("buttonConnexion")
-connexionButton.addEventListener("click", (event) => {
-    event.preventDefault();
-let emailForm = document.formLogin.email.value;
-let passwordForm = document.formLogin.password.value;
-}) 
+const connexionButton = document.getElementById("connexionButton");
+connexionButton.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  let formData = {
+    "email": document.formLogin.email.value,
+    "password": document.formLogin.password.value
+  };
+
+  let response = await fetch('http://localhost:5678/api/users/login', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(formData)
+  })
+let result = await response.json();
+console.log(result.token);
+
+document.cookie=`Login=${access_token=result.token}`
+
+window.location.href='http://127.0.0.1:5501/FrontEnd/'
+});
